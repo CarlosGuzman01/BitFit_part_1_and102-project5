ac109bce8a52d22ec60f5ef8eb9ba5270c1def01
CarlosGuzman01 <carlosguzmantejeda13@gmail.com>
2024-03-31 19:25:36 -0400

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "BitFit part 1 and102-project5"
+include(":app")
